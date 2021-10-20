fda8de2fd47cf9efb627e2655ec0dd3f7c36abf3
HEIYAN <heiyanchow@MacBook-Pro-2.local>
2021-10-20 15:18:48 -0600

    comparison logic working

--- main.py
+++ main.py
@@ -9,3 +9,26 @@ computer = choices[randint(0, 2)]
 
 print("player chose: " + player)
 print("computer chose: " + computer)
+
+if (computer == player):
+	# tie -nothing else to compare, so it'll exit
+	print("tie! try")
+
+elif (player == "rock"):
+	if (computer == "paper"):
+		print("you lose!")
+	else:
+		print("you win!")
+
+elif (player == "paper"):
+	if (computer == "scissors"):
+		print("you lose!")
+	else:
+		print("you win!")
+
+elif (player == "scissors"):
+	if (computer == "rock"):
+		print("you lose!")
+	else:
+		print("you win!")
+		
