17a265629fc4e3f0b5dc63e9c0cd1990222652b9
HEIYAN <heiyanchow@MacBook-Pro-2.local>
2021-11-06 14:23:37 -0600

    coloring text

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ while gameVars.player is False:
     gameVars.player = input("Get your weapon: rock✊, paper✋ or scissors✌️: ")
     gameVars.computer = gameVars.choices[randint(0, 2)]
 
-    print("player chose: " + gameVars.player)
-    print("computer chose: " + gameVars.computer)
+    print("Player chose: " + gameVars.player)
+    print("Computer chose: " + gameVars.computer)
 
     compare.choiceCompare()
 
-    print("player life count: " + str(gameVars.playerLives))
-    print("computer life count: " + str(gameVars.computerLives))
+    print("Player life count: " + str(gameVars.playerLives))
+    print("Computer life count: " + str(gameVars.computerLives))
 
     if gameVars.playerLives == 0:
-        winLose.winorlose("lost ｡･ﾟ･(ﾉД`)･ﾟ･｡ ")
+        winLose.winorlose("lost ｡･ﾟ･(ﾉД`)･ﾟ･｡ \033[0;0m")
 
     elif gameVars.computerLives == 0:
-        winLose.winorlose("won ✧*｡٩(ˊᗜˋ*)و✧*｡ ")
+        winLose.winorlose("won ✧*｡٩(ˊᗜˋ*)و✧*｡ \033[0;0m")
 
     gameVars.player = False
